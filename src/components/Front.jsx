ba7c2ebc258d4d1c3f7a0fb3dad8ba17b2276413
Dan Gleason <dannycgleason@gmail.com>
2023-03-28 11:18:06 -0400

    updated logo color

--- src/components/Front.jsx
+++ src/components/Front.jsx
@@ -488,7 +488,7 @@ const options = {
                     <span className="sr-only">Bundl Books</span>
                     <Image
                       className="h-6 w-auto"
-                      src='https://upload.wikimedia.org/wikipedia/commons/1/10/Red_Modern_Dachshund_Instagram_Post_%283%29.png'
+                      src='https://upload.wikimedia.org/wikipedia/commons/5/59/Bundl_Books_Logo_%281%29.png'
                       alt=""
                       height='6'
                       width='6' />
@@ -766,7 +766,7 @@ const options = {
     </nav> */}
                 <div className="w-9/12 h-9/12 object-center object-cover mb-10 ml-14 mx-25">
                   <Image
-                    src='https://upload.wikimedia.org/wikipedia/commons/1/10/Red_Modern_Dachshund_Instagram_Post_%283%29.png'
+                    src='https://upload.wikimedia.org/wikipedia/commons/5/59/Bundl_Books_Logo_%281%29.png'
                     alt={product.imageAlt}
                     //className=" object-center object-cover mb-10 ml-14 mx-25"
                     width='500'
@@ -1167,7 +1167,7 @@ const options = {
                 {/* Image section */}
                 <div className="col-span-1 md:col-span-2 lg:row-start-1 lg:col-start-1 h-8 w-8">
                   <Image
-                    src='https://upload.wikimedia.org/wikipedia/commons/1/10/Red_Modern_Dachshund_Instagram_Post_%283%29.png'
+                    src='https://upload.wikimedia.org/wikipedia/commons/5/59/Bundl_Books_Logo_%281%29.png'
                     alt=""
                     className="h-8 w-auto"
                     height='8'
