ea45dfa39c06e6af18421da12007df4eb7233a0e
Dan Gleason <dannycgleason@gmail.com>
2023-03-28 10:11:36 -0400

    removed error

--- src/pages/sitemap.xml.jsx
+++ src/pages/sitemap.xml.jsx
@@ -10,7 +10,7 @@ export const getServerSideProps = async ({ res }) => {
 
     const sitemap = `
     <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" 
-    xmlns:news="http://www.google.com/schemas/sitemap-news/0.9" x
+    xmlns:news="http://www.google.com/schemas/sitemap-news/0.9" 
     mlns:xhtml="http://www.w3.org/1999/xhtml" 
     xmlns:mobile="http://www.google.com/schemas/sitemap-mobile/1.0" 
     xmlns:image="http://www.google.com/schemas/sitemap-image/1.1" 
