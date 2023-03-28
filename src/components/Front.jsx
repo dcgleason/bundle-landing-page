6ed3dc25e1abf74f2ae4ac1be2cc5e02eeac2019
Dan Gleason <dannycgleason@gmail.com>
2023-03-28 11:33:11 -0400

    removed her references

--- src/components/Front.jsx
+++ src/components/Front.jsx
@@ -991,7 +991,7 @@ const options = {
                     <path strokeLinecap="round" strokeLinejoin="round" d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222" />
                   </svg>
                   <h3 className="mt-6 text-base font-medium text-gray-900">Graduations</h3>
-                  <p className="mt-3 text-base text-gray-500">Congratulate someone on his or her accomplishments.</p>
+                  <p className="mt-3 text-base text-gray-500">Congratulate someone on his accomplishments.</p>
                 </div>
                 <div>
                 <svg xmlns="http://www.w3.org/2000/svg" className="h-24 w-auto text-[#8B0000]" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
@@ -1006,7 +1006,7 @@ const options = {
                 <path strokeLinecap="round" strokeLinejoin="round" d="M12 8v13m0-13V6a2 2 0 112 2h-2zm0 0V5.5A2.5 2.5 0 109.5 8H12zm-7 4h14M5 12a2 2 0 110-4h14a2 2 0 110 4M5 12v7a2 2 0 002 2h10a2 2 0 002-2v-7" />
               </svg>
                   <h3 className="mt-6 text-base font-medium text-gray-900">Weddings</h3>
-                  <p className="mt-3 text-base text-gray-500">Give a the bride (or groom) a gift she or he can cherish for years to come.</p>
+                  <p className="mt-3 text-base text-gray-500">Give a the groom a gift he can cherish for years to come.</p>
                 </div>
                 <div>
                 <svg xmlns="http://www.w3.org/2000/svg" className="h-24 w-auto text-[#8B0000]" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
@@ -1020,7 +1020,7 @@ const options = {
                 <path strokeLinecap="round" strokeLinejoin="round" d="M21 13.255A23.931 23.931 0 0112 15c-3.183 0-6.22-.62-9-1.745M16 6V4a2 2 0 00-2-2h-4a2 2 0 00-2 2v2m4 6h.01M5 20h14a2 2 0 002-2V8a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
                 </svg>
                   <h3 className="mt-6 text-base font-medium text-gray-900">Retirement</h3>
-                  <p className="mt-3 text-base text-gray-500">Offer well-wishes to a co-worker on his or her final day.</p>
+                  <p className="mt-3 text-base text-gray-500">Offer well-wishes to a co-worker on his final day.</p>
                 </div>
 
                 <div>
