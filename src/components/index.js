99a6a6c2c788110aa51031252fc1530683c4c918
Dan Gleason <dannycgleason@gmail.com>
2023-03-28 11:37:15 -0400

    fixed error

--- src/components/index.js
+++ src/components/index.js
@@ -5,7 +5,6 @@ export { default as Shipping } from './Shipping'
 export { default as  HubspotContactForm } from './Form';
 export { default as  NewsletterContactForm } from './Newsletter';
 export { default as Contributors } from './Contributors';
-export { default as AILandingPage } from './AILandingPage';
 export { default as Instructions } from './Instructions';
 
 
