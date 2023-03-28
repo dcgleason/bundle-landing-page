b133ff289ec86c8506808c5a024f8da49fe17448
Dan Gleason <dannycgleason@gmail.com>
2023-03-28 11:41:19 -0400

    updated middle slogan

--- src/components/Front.jsx
+++ src/components/Front.jsx
@@ -961,7 +961,7 @@ const options = {
               <div className="flex flex-col items-center pb-10 text-center">
               <div className="max-w-xl mb-10 md:mx-auto sm:text-center lg:max-w-2xl md:mb-12">
       <h2 className="max-w-lg mb-6 font-sans text-3xl font-bold leading-none tracking-tight text-gray-900 sm:text-4xl md:mx-auto">
-           Show him how much he means to his family, friends, and you:
+           Show him (or her) how much he means to his family, friends, and you:
       </h2>{" "}
       <p className="text-base text-gray-700 md:text-lg">
         Give a Bundl book with our app on the following occasions:
