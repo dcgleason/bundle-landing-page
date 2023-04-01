87773cff1fa062d85159498f4ff059ea3b61026f
Dan Gleason <dannycgleason@gmail.com>
2023-04-01 19:48:23 -0400

    update to filed

--- src/pages/gift-idea-generator.jsx
+++ src/pages/gift-idea-generator.jsx
@@ -239,7 +239,7 @@ const [apiResponse, setApiResponse] = useState('');
             </div>
           </div>
           <div className="sm:col-span-2">
-            <label htmlFor="email" className="block text-sm font-semibold leading-6 text-gray-900">
+            <label className="block text-sm font-semibold leading-6 text-gray-900">
               What occasion is this gift for?
             </label>
             <div className="mt-2.5">
@@ -265,7 +265,7 @@ const [apiResponse, setApiResponse] = useState('');
                 id="dollar-amout"
                 value={budget}
                 onChange={(e) => setBudget(e.target.value)}
-                className="block w-full rounded-md border-0 py-2 px-3.5 pl-20 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                className="block w-full rounded-md border-0 py-2 px-3.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
               />
             </div>
           </div>
