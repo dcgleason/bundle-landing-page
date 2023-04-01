31155c34a8ebd6efe198bec6e7fea7341b6d8e11
Dan Gleason <dannycgleason@gmail.com>
2023-04-01 13:51:00 -0400

    updated end point

--- src/pages/gift-idea-generator.jsx
+++ src/pages/gift-idea-generator.jsx
@@ -38,7 +38,7 @@ const [apiResponse, setApiResponse] = useState('');
     };
   
     try {
-      const response = await axios.post('https://givebundl.com/openai/gift', formData, {
+      const response = await axios.post('https://yay-api.herokuapp.com//openai/gift', formData, {
         headers: {
           'Content-Type': 'application/json',        },
       });
