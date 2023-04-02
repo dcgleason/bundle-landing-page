7fc86f5454dd5abeb4313a7915d927e2403c8207
Dan Gleason <dannycgleason@gmail.com>
2023-04-02 09:44:02 -0400

    update

--- src/components/Front.jsx
+++ src/components/Front.jsx
@@ -961,11 +961,11 @@ const options = {
               <div className="flex flex-col items-center pb-10 text-center">
               <div className="max-w-xl mb-10 md:mx-auto sm:text-center lg:max-w-2xl md:mb-12">
       <h2 className="max-w-lg mb-6 font-sans text-3xl font-bold leading-none tracking-tight text-gray-900 sm:text-4xl md:mx-auto">
-           Show your friend/gf/bf/spouse how much they means to their family, friends, and you:
+      Give a Bundl book with our app on the following occasions:
       </h2>{" "}
-      <p className="text-base text-gray-700 md:text-lg">
+      {/* <p className="text-base text-gray-700 md:text-lg">
         Give a Bundl book with our app on the following occasions:
-      </p>
+      </p> */}
     </div>
                 {/* <h2 id="details-heading" className="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">
                 Great times to give a Bundle book:
