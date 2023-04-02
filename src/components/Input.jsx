c9a41be9e2fdd4fe75f3d6c2734986d852dc640b
Dan Gleason <dannycgleason@gmail.com>
2023-04-02 12:27:23 -0400

    spelling

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -397,7 +397,7 @@ const handleClick = () => {
             Order your Bundl book
           </h1>
           <div className="text-m pt-2 pb-2 text-center font-small text-gray-800">
-            A Bundl concierge will collect all necessary information (shipping address, Bundl Book contributor email addressses, etc.) in a subsequent email to you and will continue to coordinate the creation of your Bundl book gift. 
+            A Bundl concierge will collect all necessary information (shipping address, Bundl book contributor email addressses, etc.) in a subsequent email to you and will continue to coordinate the creation of your Bundl book gift. 
          </div>
           <h2 className="text-xl pt-3 pb-3 text-center underline font-medium text-gray-800">
             Your Information
