835d226f36efece4a31e855b93c979cc003c609c
Dan Gleason <dannycgleason@gmail.com>
2023-06-13 19:13:54 -0400

    update

--- src/components/Front.jsx
+++ src/components/Front.jsx
@@ -985,7 +985,7 @@ export default function Front() {
         <div className="w-full sm:w-2/4 py-12 text-center"> {/* text-center added to center the button */}
           <a href="https://usebundl.com/signup">
             <button className="bg-red-500 hover:bg-red-700 text-white font-bold py-4 px-8 text-2xl rounded">
-              Start Your Bundl (no credit card required)
+              Start Your Bundl Now (no credit card required)
             </button>
           </a>
         </div>
