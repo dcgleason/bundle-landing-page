634cd6c311f4227768507a79078284ebe386d1f5
Dan Gleason <dannycgleason@gmail.com>
2023-06-22 09:00:44 -0400

    update

--- src/components/Front.jsx
+++ src/components/Front.jsx
@@ -284,8 +284,10 @@ function classNames(...classes) {
 
 }
 
+
 function WaitlistForm() {
   const [email, setEmail] = useState('');
+  const [showModal, setShowModal] = useState(false);
 
   const handleSubmit = (event) => {
     event.preventDefault();
@@ -301,6 +303,7 @@ function WaitlistForm() {
       .then((data) => {
         console.log('Success:', data);
         setEmail('');
+        setShowModal(true); // Show the modal after successful submission
       })
       .catch((error) => {
         console.error('Error:', error);
@@ -324,6 +327,38 @@ function WaitlistForm() {
           Join Waitlist
         </button>
       </form>
+
+      {showModal && (
+        <div className="fixed z-10 inset-0 overflow-y-auto">
+          <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+            <div className="fixed inset-0 transition-opacity" aria-hidden="true">
+              <div className="absolute inset-0 bg-gray-500 opacity-75"></div>
+            </div>
+            <span className="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">&#8203;</span>
+            <div className="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
+              <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+                <div className="sm:flex sm:items-start">
+                  <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+                    <h3 className="text-lg leading-6 font-medium text-gray-900" id="modal-title">
+                      Success
+                    </h3>
+                    <div className="mt-2">
+                      <p className="text-sm text-gray-500">
+                        You have been successfully added to the waitlist.
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
+                <button type="button" className="mt-3 w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm" onClick={() => setShowModal(false)}>
+                  Close
+                </button>
+              </div>
+           </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
