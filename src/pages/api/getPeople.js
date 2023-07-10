663127639a664d41bbcb33b2ad0b3b46676c7112
Dan Gleason <dannycgleason@gmail.com>
2023-07-10 13:47:52 -0400

    added to erroe message

--- src/pages/api/getPeople.js
+++ src/pages/api/getPeople.js
@@ -5,6 +5,7 @@ import { google } from 'googleapis';
 export default async function handler(req, res) {
   try {
     const tokens = JSON.parse(req.headers.authorization.replace('Bearer ', ''));
+    console.log('Tokens:', tokens); // Log the tokens
 
     const oauth2Client = new google.auth.OAuth2();
     oauth2Client.setCredentials(tokens);
@@ -17,7 +18,7 @@ export default async function handler(req, res) {
 
     res.json(connections.data.connections);
   } catch (error) {
-    console.error('Failed to list connections:', error);
-    res.status(500).json({ error: 'Failed to list connections' });
+    console.error('Failed to list connections:', error); // Log the error message
+    res.status(500).json({ error: 'Failed to list connections', message: error.message }); // Include the error message in the response
   }
 }
