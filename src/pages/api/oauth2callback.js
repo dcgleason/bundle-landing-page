56637f723ccc2f8209830c9ab825d6facb9502d6
Dan Gleason <dannycgleason@gmail.com>
2023-07-10 10:47:07 -0400

    update to google api

--- src/pages/api/oauth2callback.js
+++ src/pages/api/oauth2callback.js
@@ -15,7 +15,7 @@ export default async function handler(req, res) {
   const oauth2Client = new google.auth.OAuth2(
     process.env.GOOGLE_ID,
     process.env.GOOGLE_SECRET,
-    'https://www.console.givebundl.com/api/oauth2callback'
+    'https://www.givebundl.com/api/oauth2callback'
   );
 
   try {
