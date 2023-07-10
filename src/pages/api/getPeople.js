a9f206f5983a37742281369b221fd4e4a32facbf
Dan Gleason <dannycgleason@gmail.com>
2023-07-10 13:35:26 -0400

    update to error handlign

--- src/pages/api/getPeople.js
+++ src/pages/api/getPeople.js
@@ -3,16 +3,21 @@
 import { google } from 'googleapis';
 
 export default async function handler(req, res) {
-  const tokens = JSON.parse(req.headers.authorization.replace('Bearer ', ''));
+  try {
+    const tokens = JSON.parse(req.headers.authorization.replace('Bearer ', ''));
 
-  const oauth2Client = new google.auth.OAuth2();
-  oauth2Client.setCredentials(tokens);
+    const oauth2Client = new google.auth.OAuth2();
+    oauth2Client.setCredentials(tokens);
 
-  const people = google.people({ version: 'v1', auth: oauth2Client });
-  const connections = await people.people.connections.list({
-    resourceName: 'people/me',
-    personFields: 'names,emailAddresses',
-  });
+    const people = google.people({ version: 'v1', auth: oauth2Client });
+    const connections = await people.people.connections.list({
+      resourceName: 'people/me',
+      personFields: 'names,emailAddresses',
+    });
 
-  res.json(connections.data.connections);
+    res.json(connections.data.connections);
+  } catch (error) {
+    console.error('Failed to list connections:', error);
+    res.status(500).json({ error: 'Failed to list connections' });
+  }
 }
