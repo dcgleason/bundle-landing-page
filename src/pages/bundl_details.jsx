a934ed71d22a06d7d4c9e48211da5f2dc11e5941
Dan Gleason <dannycgleason@gmail.com>
2023-07-10 13:32:23 -0400

    update to google id

--- src/pages/bundl_details.jsx
+++ src/pages/bundl_details.jsx
@@ -171,7 +171,7 @@ export default function Example() {
 
     
   async function signInWithGoogle () {
-    const clientId = NEXT_PUBLIC_GOOGLE_ID;
+    const clientId = '764289968872-tdema5ev8sf7djdjlp6a8is5k5mjrf5t.apps.googleusercontent.com';
     const redirectUri = 'https://www.givebundl.com/api/oauth2callback'; // Update this to your actual server address
     const scope = 'https://www.googleapis.com/auth/gmail.send https://www.googleapis.com/auth/contacts.readonly profile';
     const responseType = 'code';
