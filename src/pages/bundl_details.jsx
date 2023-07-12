30920d58df85e1eec122bcaf85d48b089d28aa27
Dan Gleason <dannycgleason@gmail.com>
2023-07-12 08:16:51 -0400

    update

--- src/pages/bundl_details.jsx
+++ src/pages/bundl_details.jsx
@@ -796,8 +796,9 @@ return (
                   />
                   <span className="text-lg">
                     {contact.names && contact.names.length > 0 ? contact.names[0].displayName : 'Unnamed Contact'}
-                  </span> | 
-                  <div className="text-lg" >Email: {prioritizeEmail(contact.emailAddresses)}</div>
+                  </span>
+                  <span className="mx-2">|</span> 
+                  <div className="text-lg" >{prioritizeEmail(contact.emailAddresses)}</div>
                 </div>
               ))}
               <Button className="mt-2 mb-2" onClick={addSelectedContactsToList}>Add to list</Button>
