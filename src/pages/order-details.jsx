4a88d7091b8ef73db7c4dbddbe07ade7485debdd
Dan Gleason <dannycgleason@gmail.com>
2023-07-12 15:05:20 -0400

    update take out code

--- src/pages/order-details.jsx
+++ src/pages/order-details.jsx
@@ -196,41 +196,7 @@ useEffect(() => {
   }
 }, []);
   
-const changeHandler = (event) => {
-  // Passing file data (event.target.files[0]) to parse using Papa.parse
-  console.log('event.target.files[0]', event.target.files[0])
-  Papa.parse(event.target.files[0], {
-    header: true,
-    skipEmptyLines: true,
-    complete: function (results) {
-      const rowsArray = [];
-      const valuesArray = [];
 
-      // Iterating data to get column name and their values
-      results.data.map((d) => {
-        rowsArray.push(Object.keys(d));
-        valuesArray.push(Object.values(d));
-      });
-
-      // Parsed Data Response in array format
-      setParsedData(results.data);
-
-      // Filtered Column Names
-      setTableRows(rowsArray[0]);
-
-      // Filtered Values
-      setValues(valuesArray);
-      console.log('values = '+ values)
-      console.log('parsedData = '+ parsedData)
-
-      // Store the parsed data in localStorage
-      if (typeof window !== 'undefined') {
-        localStorage.setItem('csvData', JSON.stringify(results.data));
-      }
-    },
-  });
-  setCsvUploaded(true);
-};
 
 
 const handleContactSelect = (contact, isSelected) => {
