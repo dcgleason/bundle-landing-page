9283652cbdf025c43529bf15e935f92652c85a59
Dan Gleason <dannycgleason@gmail.com>
2023-07-12 07:22:54 -0400

    update ot page size

--- src/pages/api/getPeople.js
+++ src/pages/api/getPeople.js
@@ -15,6 +15,7 @@ export default async function handler(req, res) {
 
     const connections = await people.people.connections.list({
       resourceName: 'people/me',
+      pageSize: 2000,
       personFields: 'names,emailAddresses',
     });
 
