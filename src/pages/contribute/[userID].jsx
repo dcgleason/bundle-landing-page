924b24abd30996d0eac4eef17cc74a81921c8d92
Dan Gleason <dannycgleason@gmail.com>
2023-07-21 10:56:37 -0400

    update

--- src/pages/contribute/[userID].jsx
+++ src/pages/contribute/[userID].jsx
@@ -75,7 +75,7 @@ const Messages = () => {
   const [isLoading, setIsLoading] = useState(false);
   const [bookData, setBookData] = useState({});
   const [layoutId, setLayoutId] = useState("1");
-  const [userEmail, setUserEmail] = useState("teting@testing.com");
+  const [userEmail, setUserEmail] = useState("daniell@testing.com");
 
   useEffect(() => {
     if (submissionStatus) {
